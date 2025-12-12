d38a823c5360b190f61b9bb8cde0d1da699e3a3f
CRYPTOBIZON <32658127+ettirammstein@users.noreply.github.com>
2025-12-12 12:29:46 +0300

    Update lib.rs

--- Contract/src/lib.rs
+++ Contract/src/lib.rs
@@ -1,1 +1,25 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::near_bindgen;
 
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    value: u64,
+}
+
+impl Default for Contract {
+    fn default() -> Self {
+        Self { value: 0 }
+    }
+}
+
+#[near_bindgen]
+impl Contract {
+    pub fn get_value(&self) -> u64 {
+        self.value
+    }
+
+    pub fn set_value(&mut self, value: u64) {
+        self.value = value;
+    }
+}
